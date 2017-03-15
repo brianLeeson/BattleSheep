d1aac5607d9c10488cbef3769deb6e1a2dee5acb
Holden <marmotmarsh74@gmail.com>
2017-03-14 22:46:36 -0700

    Added cell connections

--- board.cpp
+++ board.cpp
@@ -1,5 +1,6 @@
 #include "board.h"
 #include "game.h"
+#include <iostream>
 
 extern Game* game;
 
@@ -15,23 +16,44 @@ void Board::placeSpaces(int x, int y, int cols, int rows){
     int X_SHIFT = 70;
     int Y_SHIFT = 41;
 
-    std::vector<Space*> prevColumn;
+    std::vector<Space*> curColumn;
 
     for (size_t i = 0, n = cols; i < n; i++){
-        if (i % 2 == 0){ // even column
+        if (i % 2 == 0) { // even column
             Y_SHIFT = 0;
-        }
-        else{ // odd column
+        } else { // odd column
             Y_SHIFT = 41;
         }
-        prevColumn = createSpaceColumn(x+X_SHIFT*i,y+Y_SHIFT,rows);
+        curColumn = createSpaceColumn(x+X_SHIFT*i,y+Y_SHIFT,rows);
+
+        // connect spaces diagonally
+        if (i != 0) {
+            if (i%2 == 0) { // even column
+                for (size_t j = 0; j < rows; j++) {
+                    spaces[(rows*i)+j]->setAdjacent("dl", spaces[(rows*(i-1))+j]);
+                    spaces[(rows*(i-1))+j]->setAdjacent("ur", spaces[(rows*i)+j]);
+                    if (j != 0) {
+                        spaces[(rows*i)+j]->setAdjacent("ul", spaces[(rows*(i-1))+j-1]);
+                        spaces[(rows*(i-1))+j-1]->setAdjacent("dr", spaces[(rows*i)+j]);
+                    }
+                }
+            } else { // odd column
+                for (size_t j = 0, k = rows; j < k; j++) {
+                    spaces[(rows*i)+j]->setAdjacent("ul", spaces[(rows*(i-1))+j]);
+                    spaces[(rows*(i-1))+j]->setAdjacent("dr", spaces[(rows*i)+j]);
+                    if (j != (rows-1)) {
+                        spaces[(rows*i)+j]->setAdjacent("dl", spaces[(rows*(i-1))+j+1]);
+                        spaces[(rows*(i-1))+j+1]->setAdjacent("ur", spaces[(rows*i)+j]);
+                    }
+                }
+            }
+        }
     }
 }
 
 std::vector<Space*> Board::createSpaceColumn(int x, int y, int numOfRows){
     std::vector<Space*> column;
     Space* space;
-    Space* prev;
     // creates a column of Hexes at the specified location with the specified
     // number of rows
     for (size_t i = 0, n = numOfRows; i < n; i++){
@@ -40,9 +62,13 @@ std::vector<Space*> Board::createSpaceColumn(int x, int y, int numOfRows){
         column.push_back(space);
         spaces.push_back(space);
         game->scene->addItem(space);
-        if (i != 0) {
-            continue;
+
+        // connect Spaces top to bottom
+        if (i != 0 ) {
+            column[i-1]->setAdjacent("d", column[i]);
+            column[i]->setAdjacent("u", column[i-1]);
         }
     }
+
     return column;
 }
